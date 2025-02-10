f24f487186d656e84134ba838c77158c7d31d0a5
Kelsee Lawrence <klawrence1@ewu.edu>
2025-02-10 23:43:39 +0000

    gsap tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
 console.log('OK');
+
+gsap.registerPlugin(Draggable);
+
+Draggable.create(".food", {
+    bounds: "#stage",
+    onDrag: function(){
+      if (this.hitTest(".trashcan")) {
+        gsap.to(this.target, {
+            duration: 0.7, opacity: 0, scale: 0, })
+      }
+    }
+});
