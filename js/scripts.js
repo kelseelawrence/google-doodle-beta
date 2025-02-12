fde53243805f913f41e8892ebdfe6f85fccc9c47
Kelsee Lawrence <klawrence1@ewu.edu>
2025-02-12 00:12:28 +0000

    updated svg

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,72 @@
 console.log('OK');
 
-gsap.to(.globe-one", {
-    duration: 2,
-    rotation: 360,
-    repeat: -1,
-    ease: "none"
+gsap.to("#flower-one", {
+  scale : 2.5,
+  duration : 3,
+  transformOrigin : "50% 50%",
+  rotate: 360,
+  repeat : -1,
+  yoyoEase : true,
+});
+
+gsap.to("#flower-three", {
+  scale : 2.5,
+  duration : 3,
+  transformOrigin : "50% 50%",
+  rotate: 360,
+  repeat : -1,
+  yoyoEase : true,
+});
+
+gsap.to("#flower-two", {
+  scale : 2.5,
+  duration : 3,
+  transformOrigin : "50% 50%",
+  rotate: -360,
+  repeat : -1,
+  yoyoEase : true,
+});
+
+gsap.to("#flower-four", {
+  scale : 2.5,
+  duration : 3,
+  transformOrigin : "50% 50%",
+  rotate: -360,
+  repeat : -1,
+  yoyoEase : true,
+});
+
+gsap.to("#sun", {
+  rotate: 360,
+  transformOrigin: "50% 50%",
+  duration: 3,
+  repeat: -1,
 })
 
-gsap.registerPlugin(Draggable);
+gsap.to("#right-globe", {
+  rotate: 360,
+  duration: 5,
+  transformOrigin: "50% 50%",
+  repeat: -1,
+        })
+
+gsap.to("#left-globe", {
+  rotate: -360,
+  duration: 5,
+  transformOrigin: "50% 50%",
+  repeat: -1,
+        })
+
+gsap.to(".grass", {
+  ease: "bounce.in",
+  duration: 2.5,
+        })
 
 Draggable.create(".food", {
-    bounds: "#stage",
-    onDrag: function(){
-      if (this.hitTest(".trashcan")) {
-        gsap.to(this.target, {
-            duration: 0.7, opacity: 0, scale: 0, })
-      }
+  bounds: "svg",
+  onDrag: function() {
+    if(this.hitTest("#trashcan")) {
+      gsap.to(this.target, {duration: 1.0, opacity:0.5, scale:0 })
     }
+  }
 });
