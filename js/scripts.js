bfd1c0c975ed1012c541937e3be034f3517e882a
Kelsee Lawrence <klawrence1@ewu.edu>
2025-03-05 00:39:49 +0000

    added different colors for flowers to change between

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,45 @@
 document.addEventListener('DOMContentLoaded', function() {
   console.log('OK');
 
+  // Array of possible flower colors
+  const flowerColors = [
+    '#fbb26d',
+    '#f57d62', 
+    '#e15b64', 
+    '#abbd83', 
+    '#E05263', 
+    '#681C42', 
+    '#D20074', 
+    '#F7A814', 
+    '#DF253C', 
+    '#C84D8E', 
+    '#DC6EA8', 
+    '#FCC6E9', 
+  ];
+
+  // Apply a random color to each flower
+  document.querySelectorAll('.flower').forEach(flower => {
+    const getRandNum = Math.floor(Math.random() * flowerColors.length);
+    const selectedColor = flowerColors[getRandNum];
+    flower.setAttribute('fill', selectedColor);
+    flower.setAttribute('stroke', '#000'); // Ensure the stroke is visible
+  });
+
   gsap.to("#flower-one", {
     scale: 1.8, // Reduced scale
     duration: 4,
     transformOrigin: "50% 50%",
     rotate: 15, // Just a small tilt
+    yoyo: true,
+    repeat: -1,
+    ease: "power1.inOut",
+  });
+
+  gsap.to("#flower-two", {
+    scale: 1.5,
+    duration: 5,
+    transformOrigin: "50% 50%",
+    opacity: 0.7, // Adding a subtle fade
     yoyo: true,
     repeat: -1,
     ease: "power1.inOut",
@@ -21,13 +55,14 @@ document.addEventListener('DOMContentLoaded', function() {
     ease: "power1.inOut",
   });
 
-  gsap.to("#flower-two", {
-    scale: 1.5,
-    duration: 5,
+  gsap.to("#flower-four", {
+    scale: 1.6,  // Adjusted scale
+    duration: 4.2,  // Duration of the animation
     transformOrigin: "50% 50%",
-    opacity: 0.7, // Adding a subtle fade
-    yoyo: true,
-    repeat: -1,
+    rotate: 20,  // Rotate by 20 degrees
+    opacity: 0.8,  // Make it slightly less opaque
+    yoyo: true,  // The animation will reverse at the end
+    repeat: -1,  // Infinite loop
     ease: "power1.inOut",
   });
 
